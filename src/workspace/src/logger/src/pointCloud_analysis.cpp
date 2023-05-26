5b071ce0af79c4836b736f27135940cc8be63ce1
pat <patrick.cmorneau@cidco.ca>
2023-05-26 15:22:28 -0400

    unit vector

--- src/workspace/src/logger/src/pointCloud_analysis.cpp
+++ src/workspace/src/logger/src/pointCloud_analysis.cpp
@@ -1,13 +1,14 @@
 #include <SbetProcessor.hpp>
 #include <filesystem>
 #include "../../../MBES-lib/src/Position.hpp"
-#include "../../../MBES-lib/src/Attitude.hpp"
+#include "../../../MBES-lib/src/math/CoordinateTransform.hpp"
+
 #include <iostream>
 #include <fstream>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
 #include <pcl/kdtree/kdtree_flann.h>
-#include <pcl/filters/crop_box.h>
+//#include <pcl/filters/crop_box.h>
 #include <pcl/visualization/cloud_viewer.h>
 
 
@@ -19,12 +20,14 @@ class PointCloudAnalyz0r : public SbetProcessor{
 		
 		~PointCloudAnalyz0r(){}
 		
+		
 		void processEntry(SbetEntry * entry){
 			
 			Position position(static_cast<uint64_t>(entry->time * 1000000), entry->latitude, entry->longitude, entry->altitude);
 			
 			this->sbetPositions.push_back(position);
 		}
+		
 		
 		void readPointCloud(){
 			
@@ -53,51 +56,75 @@ class PointCloudAnalyz0r : public SbetProcessor{
 				this->kdtree.setInputCloud (cloud);
 			}
 			else{
-				std::cerr<<"Invalid point cloud file path." << std::endl;
+				std::cerr<<"Invalid path for point cloud file." << std::endl;
 			}
 		}
+		
 		
 		void readSbet(){
 			readFile(this->sbetFilePath);
 		}
 		
+		
 		void read(){
 			readPointCloud();
 			readSbet();
 			
-			//std::cerr<<pointCloudXs.size() << " points read && " << sbetPositions.size() << " positions read" << std::endl;
+			Position firstPos = this->sbetPositions.at(0);	
+			
+			double firstLat = firstPos.getLatitude();
+			double firstLon = firstPos.getLongitude();
+			
+			setEcef2EnuMatrix(firstLat, firstLon);
+			
+			setFirstPositionEcef(firstPos);
+			
+			//std::cout<< "first pos ecef: "<<   this->firstPositionECEF<<"\n";
 		}
+		
+		
+		void setFirstPositionEcef(Position & firstPosition){
+			CoordinateTransform::getPositionECEF(this->firstPositionECEF, firstPosition);
+		}
+		
+		
+		Eigen::Vector3d computeUnitVector(Position & position1, Position & position2){
+			
+			Eigen::Vector3d positionLGF1;
+			position2lgf(positionLGF1, position1);
+			
+			Eigen::Vector3d positionLGF2;
+			position2lgf(positionLGF2, position2);
+			
+			//std::cout<< "pos1: \n"<<positionLGF1<<"\n\n";
+			//std::cout<< "pos2: \n"<<positionLGF2<<"\n\n";
+			
+			Eigen::Vector3d vector = positionLGF2 - positionLGF1;
+			
+			Eigen::Vector3d unitVector = vector / vector.norm(); //TODO 
+			
+			std::cout<<"unit vector: \n" << unitVector <<"\n\n";
+			
+			return unitVector;
+		}
+		
 		
 		void denoise(){
-			/*
-			pcl::CropBox<pcl::PointXYZ> box(true); //TODO set to false when dev is done
 			
-			int minIndex = 0;
-			int maxIndex = 10;
-			while(maxIndex < sbetPositions.size()-1){
+			for(long unsigned int i = 0; i < this->sbetPositions.size(); i=i+100){
+				Position pos1 = this->sbetPositions.at(i);
+				Position pos2 = this->sbetPositions.at(i+100); //TODO make step a param
 				
-				Position min = sbetPositions.at(minIndex);
-				Position max = sbetPositions.at(maxIndex);
-			
-				//Eigen::Vector4f minBoxPoint();
-				//Eigen::Vector4f maxBoxPoint;
-				//box.setMin()
+				Eigen::Vector3d unitVector = computeUnitVector(pos1, pos2); 
 				
+				break;
 				
-				minIndex += 10;
-				maxIndex += 10;
-				if(maxIndex > sbetPositions.size()-1){
-					maxIndex = sbetPositions.size()-1;
-				}
-				if(minIndex >= sbetPositions.size()-1 || minIndex == maxIndex){
-					break;
-				}
 			}
-			*/
-			
 		}
 		
+		
 		void done(){} // needed because of sbetprocessor base class
+		
 		
 		void showPointCloud(){
 			pcl::visualization::CloudViewer viewer ("Simple Cloud Viewer");
@@ -107,11 +134,35 @@ class PointCloudAnalyz0r : public SbetProcessor{
 			while (!viewer.wasStopped ()){}
 		}
 		
+		
+		void position2lgf(Eigen::Vector3d & positionLGF, Position & p){
+			
+			Eigen::Vector3d positionECEF;
+			
+			CoordinateTransform::getPositionECEF(positionECEF, p);
+			
+			positionLGF = this->ecef2enu * (positionECEF - this->firstPositionECEF);
+			
+		}
+		
+		
+		void setEcef2EnuMatrix(double & firstLat, double & firstLon){
+			this->ecef2enu << -sin(firstLon*D2R), cos(firstLon*D2R), 0,
+					-sin(firstLat*D2R)*cos(firstLon*D2R), -sin(firstLat*D2R)*sin(firstLon*D2R), cos(firstLat*D2R),
+					cos(firstLat*D2R)*cos(firstLon*D2R), cos(firstLat*D2R)*sin(firstLon*D2R), sin(firstLat*D2R);
+			
+			this->ecef2enu.transposeInPlace();
+		}
+		
 	private:
 		std::string sbetFilePath;
 		std::string pointCloudFilePath;
 		
 		std::vector<Position> sbetPositions;
+		
+		Eigen::Matrix3d ecef2enu;
+		Eigen::Vector3d firstPositionECEF;
+		
 		pcl::PointCloud<pcl::PointXYZ>::Ptr cloud; 
 		pcl::KdTreeFLANN<pcl::PointXYZ> kdtree;
 };
@@ -128,7 +179,11 @@ int main(int argc,char** argv){
 		
 		PointCloudAnalyz0r pca(sbetFilePath, pointCloudFilePath);
 		pca.read();
+		
+		pca.denoise();
+		
 		//pca.showPointCloud();
+		
 		
 	}
 	
