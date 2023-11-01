f5372c08781d8282fb9b30147022ac711d4ffb72
patrick <patcmorneau@cidco.ca>
2023-11-01 14:57:56 -0400

    ...

--- src/workspace/src/diagnostics/include/diagnostics/ClockDiagnostic.cpp
+++ src/workspace/src/diagnostics/include/diagnostics/ClockDiagnostic.cpp
@@ -1,28 +1,55 @@
 #include <libgpsmm.h>
+#include <gps.h>
 #include <iostream>
 using namespace std;
 
+
+static void libgps_dump_state(struct gps_data_t *data){
+	std::cout<<"ok2 \n";
+	/*
+    char ts_str[TIMESPEC_LEN];
+
+    if (data->set & TIME_SET) {
+        (void)fprintf(stdout, "TIME: %s\n",
+                      timespec_str(&data->fix.time, ts_str, sizeof(ts_str)));
+    }
+	*/
+	
+	struct timespec * ts;
+	ts = &data->fix.time;
+	char buff[100];
+    strftime(buff, sizeof buff, "%D %T", gmtime(&ts->tv_sec));
+    printf("Current time: %s.%09ld UTC\n", buff, ts->tv_nsec);
+//    printf("Raw timespec.time_t: %jd\n", (intmax_t)ts.tv_sec);
+//    printf("Raw timespec.tv_nsec: %09ld\n", ts.tv_nsec);
+	
+	
+}
+
+
+
 int main(void)
 {
-    gpsmm gps_rec("localhost", DEFAULT_GPSD_PORT);
+    gpsmm gps_rec("localhost", "2947");
 
-    if (gps_rec.stream(WATCH_ENABLE|WATCH_NEWSTYLE) == NULL) {
+    if (gps_rec.stream(WATCH_ENABLE|WATCH_JSON) == NULL) {
         cerr << "No GPSD running.\n";
         return 1;
     }
 
     for (;;) {
-	struct gps_data_t* newdata;
+		struct gps_data_t* newdata;
 
-	if (!gps_rec.waiting(50000000))
-	  continue;
+		if (!gps_rec.waiting(50000000))
+		  continue;
 
-	if ((newdata = gps_rec.read()) == NULL) {
-	    cerr << "Read error.\n";
-	    return 1;
-	} else {
-	    //PROCESS(newdata);
-	}
+		if ((newdata = gps_rec.read()) == NULL) {
+			cerr << "Read error.\n";
+			return 1;
+		} else {
+			std::cout<<"ok\n";
+			libgps_dump_state(newdata);
+		}
     }
     return 0;
 }
