8c3d040c8ed98bc6934711c212ef4f2955adea92
patrick <patcmorneau@cidco.ca>
2023-11-02 10:52:53 -0400

    please be that

--- src/workspace/src/diagnostics/include/diagnostics/ClockDiagnostic.cpp
+++ src/workspace/src/diagnostics/include/diagnostics/ClockDiagnostic.cpp
@@ -4,27 +4,6 @@
 using namespace std;
 
 
-static void libgps_dump_state(struct gps_data_t *data){
-	/*
-	char ts_str[TIMESPEC_LEN];
-
-	if (data->set & TIME_SET) {
-		(void)fprintf(stdout, "TIME: %s\n",
-					  timespec_str(&data->fix.time, ts_str, sizeof(ts_str)));
-	}
-	*/
-	
-	struct timespec * ts;
-	ts = &data->fix.time;
-	char buff[100];
-	strftime(buff, sizeof buff, "%D %T", gmtime(&ts->tv_sec));
-	printf("Current time: %s.%09ld UTC\n", buff, ts->tv_nsec);
-//	printf("Raw timespec.time_t: %jd\n", (intmax_t)ts.tv_sec);
-//	printf("Raw timespec.tv_nsec: %09ld\n", ts.tv_nsec);
-}
-
-
-
 int main(void){
 	gpsmm gps_rec("localhost", "2947");
 
@@ -34,20 +13,17 @@ int main(void){
 	}
 
 	for (;;) {
-		struct gps_data_t* newdata;
-		//if (gps_rec.waiting(50000000)){
-			struct gps_data_t* gpsData = gps_rec.read();
-			if (gpsData != nullptr) {
-				//if (gpsData->status >= 2) { // Check if the GPS fix is valid
-					struct timespec * ts;
-					ts = &gpsData->fix.time;
-					cout << "GPS Time: " << ts << std::endl;
-				//}
-			}
-			else{
-				cerr<<"Read error \n";
-			}
-		//}
+		struct gps_data_t* gpsData = gps_rec.read();
+		if (gpsData != nullptr) {
+			struct timespec ts;
+			ts = gpsData->fix.time;
+			//cout << "GPS Time: " << ts << std::endl;
+			char buff[100];
+			strftime(buff, sizeof buff, "%D %T", gmtime(&ts.tv_sec));
+			printf("Current time: %s.%09ld UTC\n", buff, ts.tv_nsec);
+		}
+
+
 	}
 	return 0;
 }
