6cd4960ccbfe5b76cd0a7aea26f6174ce8f323a7
patrick <patcmorneau@cidco.ca>
2024-08-13 14:43:16 -0400

    3 ms after reception of the Sonar Return Data

--- src/workspace/src/sonar_imagenex852_c/src/main.cpp
+++ src/workspace/src/sonar_imagenex852_c/src/main.cpp
@@ -171,7 +171,9 @@ class Imagenex852{
 					
 					ros::Duration(0.003).sleep();
 					send_command();
-					ros::Duration(0.0023).sleep();
+					ros::Duration(0.003).sleep();
+					send_command();
+					ros::Duration(0.003).sleep();
 					ros::Rate error_rate( 1 );
 					ros::Rate loop_rate( 1 );
 					while(ros::ok()){
@@ -214,71 +216,6 @@ class Imagenex852{
 							}
 							else{
 								ROS_ERROR("serial read 1 = 0");
-								
-								tty.c_cflag &= ~PARENB; //No parity
-								tty.c_cflag &= ~CSTOPB; //One Stop bit
-								tty.c_cflag |= CS8;	 // 8 bits
-								tty.c_cflag &= ~CRTSCTS; // Disable RTS/CTS hardware flow control
-								tty.c_cflag |= CREAD | CLOCAL; // Turn on READ & ignore ctrl lines
-								tty.c_lflag &= ~ICANON;
-								tty.c_lflag &= ~ECHO; // Disable echo
-								tty.c_lflag &= ~ECHOE; // Disable erasure
-								tty.c_lflag &= ~ECHONL; // Disable new-line echo
-								tty.c_lflag &= ~ISIG; // Disable interpretation of INTR, QUIT and SUSP
-								tty.c_iflag &= ~(IXON | IXOFF | IXANY); // Turn off s/w flow ctrl
-								tty.c_iflag &= ~(IGNBRK|BRKINT|PARMRK|ISTRIP|INLCR|IGNCR|ICRNL); // Disable any special handling of received bytes
-								tty.c_oflag &= ~OPOST; // Prevent special interpretation of output bytes (e.g. newline chars)
-								tty.c_oflag &= ~ONLCR; // Prevent conversion of newline to carriage return/line feed
-								tty.c_cc[VTIME] = 1;	// Wait for up to 25s (250 deciseconds), returning as soon as any data is received.
-								tty.c_cc[VMIN] = 0;
-
-								cfsetispeed(&tty, B115200);
-								cfsetospeed(&tty, B115200);
-				
-								if(tcsetattr(deviceFile,TCSANOW,&tty) != 0){
-									ROS_ERROR("Error while configuring serial port %s : %s",devicePath.c_str(),strerror(errno));
-									throw std::invalid_argument(strerror(errno));
-								}
-								
-								
-								while(serialRead((uint8_t*)&read_buf, sizeof(read_buf)) != 1){
-									send_command();
-								}
-								
-								ROS_INFO("here 0");
-								
-								uint8_t terminationCharacter;
-								do{
-									serialRead(&terminationCharacter,sizeof(uint8_t));
-								}
-								while(terminationCharacter != 0xFC);
-								
-								
-								tty.c_cflag &= ~PARENB; //No parity
-								tty.c_cflag &= ~CSTOPB; //One Stop bit
-								tty.c_cflag |= CS8;	 // 8 bits
-								tty.c_cflag &= ~CRTSCTS; // Disable RTS/CTS hardware flow control
-								tty.c_cflag |= CREAD | CLOCAL; // Turn on READ & ignore ctrl lines
-								tty.c_lflag &= ~ICANON;
-								tty.c_lflag &= ~ECHO; // Disable echo
-								tty.c_lflag &= ~ECHOE; // Disable erasure
-								tty.c_lflag &= ~ECHONL; // Disable new-line echo
-								tty.c_lflag &= ~ISIG; // Disable interpretation of INTR, QUIT and SUSP
-								tty.c_iflag &= ~(IXON | IXOFF | IXANY); // Turn off s/w flow ctrl
-								tty.c_iflag &= ~(IGNBRK|BRKINT|PARMRK|ISTRIP|INLCR|IGNCR|ICRNL); // Disable any special handling of received bytes
-								tty.c_oflag &= ~OPOST; // Prevent special interpretation of output bytes (e.g. newline chars)
-								tty.c_oflag &= ~ONLCR; // Prevent conversion of newline to carriage return/line feed
-								tty.c_cc[VTIME] = 250;	// Wait for up to 25s (250 deciseconds), returning as soon as any data is received.
-								tty.c_cc[VMIN] = 0;
-
-								cfsetispeed(&tty, B115200);
-								cfsetospeed(&tty, B115200);
-				
-								if(tcsetattr(deviceFile,TCSANOW,&tty) != 0){
-									ROS_ERROR("Error while configuring serial port %s : %s",devicePath.c_str(),strerror(errno));
-									throw std::invalid_argument(strerror(errno));
-								}
-								
 							}
 						}
 						catch(std::exception & e){
