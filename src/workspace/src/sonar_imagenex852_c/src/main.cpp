196aed6767db624875431a3ac6691ef5ec32694a
patrick <patcmorneau@cidco.ca>
2024-08-20 11:03:20 -0400

    testing config changed

--- src/workspace/src/sonar_imagenex852_c/src/main.cpp
+++ src/workspace/src/sonar_imagenex852_c/src/main.cpp
@@ -293,7 +293,7 @@ class Imagenex852{
 		}
 		
 		void process_data(Imagenex852ReturnDataHeader hdr){
-			//ROS_INFO("%x range", hdr.range);
+			ROS_INFO("%d range", hdr.range);
 			
 			if(hdr.range == sonarRange){
 				this->configChanged = false;
